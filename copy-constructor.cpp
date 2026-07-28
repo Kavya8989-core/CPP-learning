a3af2fc538300d4fcbd3303261f0c4cba266d309
KAVYA SHARMA <ks2371008@gmail.com>
2026-07-28 21:50:15 +0530

    copy-constructor.cpp

---
+++ copy-constructor.cpp
@@ -0,0 +1,62 @@
+// #include <iostream>
+// using namespace std;
+//     class Player{
+//         public:
+//         string name;
+//         int health;
+//         Player(string n,int h){
+//             name=n;
+//             health=h;
+//         }
+//         Player(const Player &obj){
+//             cout<<"Copy constructor is called"<<endl;
+//             name=obj.name;
+//             health=obj.health;
+//         }
+//         void show(){
+//             cout<<"Name:- "<<name<<endl;
+//             cout<<"Health:- "<<health<<endl;
+//         }
+//         void sethealth(int h){
+//             health=h;
+//         }
+        
+//     };
+//     int main(){
+//         Player p1("Knight",100);
+//         Player p2=p1;
+//         p2.sethealth(50);
+//         p1.show();
+//         p2.show();
+//     }
+
+#include <iostream>
+using namespace std;
+    class Player{
+        public:
+        string name;
+        int health;
+        Player(string n,int h){
+            name=n;
+            health=h;
+        }
+        Player(const Player &obj){
+            cout<<"Copy constructor is called"<<endl;
+            name=obj.name;
+            health=obj.health;
+        }
+        void show(){
+            cout<<"Name:- "<<name<<endl;
+            cout<<"Health:- "<<health<<endl;
+        }
+        void sethealth(int h){
+            health=h;
+        }
+        
+    };
+    int main(){
+        Player p1("Knight",100);
+        Player p2=p1;
+        Player p3(p1);
+        return 0;
+    }
