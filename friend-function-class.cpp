b3356c43b363ad3cdd16fdd3bfc695e510068355
KAVYA SHARMA <ks2371008@gmail.com>
2026-07-29 08:33:57 +0530

    Friend funtion

---
+++ friend-function-class.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+    class Customer{
+        private:
+        int balance;
+        public:
+        Customer(int b){
+            balance=b;
+        }
+        friend void showBalance(Customer c);
+        
+    };
+    void showBalance(Customer c){
+            cout<<"Balance:- "<<c.balance;
+        }
+        int main(){
+            Customer c(5000);
+            showBalance(c);
+            return 0;
+        }
