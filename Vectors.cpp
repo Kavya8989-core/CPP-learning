8fe8de1c004dbd6ea01559dae0cd8172924d2354
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-02 15:17:59 +0530

    Learning vectors

---
+++ Vectors.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+    int main(){
+        vector<int> marks;
+        marks.push_back(85);
+        marks.push_back(90);
+        marks.push_back(78);
+        marks.push_back(95);
+        marks.push_back(88);
+        for(int i=0;marks.size();i++){
+            cout<<marks[i]<<endl;
+        }
+        return 0;
+    }
