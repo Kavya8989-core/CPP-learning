a1183fe78bdfe7a4c3d3144a4a6616397a1df321
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-03 00:02:47 +0530

    adding an inventory system

--- Vectors.cpp
+++ Vectors.cpp
@@ -1,15 +1,35 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+//     int main(){
+//         vector<int> marks;
+//         marks.push_back(85);
+//         marks.push_back(90);
+//         marks.push_back(78);
+//         marks.push_back(95);
+//         marks.push_back(88);
+//         for(int i=0;marks.size();i++){
+//             cout<<marks[i]<<endl;
+//         }
+//         return 0;
+//     }
+
 #include <iostream>
 #include <vector>
 using namespace std;
     int main(){
-        vector<int> marks;
-        marks.push_back(85);
-        marks.push_back(90);
-        marks.push_back(78);
-        marks.push_back(95);
-        marks.push_back(88);
-        for(int i=0;marks.size();i++){
-            cout<<marks[i]<<endl;
+        vector <string> inventory;
+        inventory.push_back("Sword");
+        inventory.push_back("Potion");
+        int choice;
+        switch (choice)
+        {
+        case 1:
+            string ;
+            cout<<"Enter item: ";
+            break;
+        
+        default:
+            break;
         }
-        return 0;
     }
