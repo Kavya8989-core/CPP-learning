247739fa66658154a00f4723823095227f7372b6
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-03 08:23:03 +0530

    updates

--- Vectors.cpp
+++ Vectors.cpp
@@ -19,26 +19,43 @@
 using namespace std;
     int main(){
         vector <string> inventory;
-        inventory.push_back("Inventory system");
-        inventory.push_back("1. Add item");
-        inventory.push_back("2. Remove last item");
-        inventory.push_back("3. Show Inventory");
-        inventory.push_back("4. Clear Iventory");
-        inventory.push_back("5. Exit");
-        inventory.push_back("Potion");
-
-        for(int i=0;inventory.size();i++){
-            cout<<inventory[i]<<endl;
+        cout<<"Inventory system"<<endl;
+        cout<<"1. Add item"<<endl;
+        cout<<"2. Remove last item"<<endl;
+        cout<<"3. Show Inventory"<<endl;
+        cout<<"4. Clear Iventory"<<endl;
+        cout<<"5. Exit"<<endl;
+        int choice;
+        cout<<"Enter your choice"<<endl;
+        cin>>choice;
+        string item;
+        switch (choice)
+        {
+        case 1:
+            cout<<"Enter item: ";
+            cin>>item;
+            inventory.push_back(item);
+            cout<<item<<" is added in your inventory"<<endl;
+            break;
+        case 2:
+        if(inventory.size()>0){
+        char decision;
+        cout<<"Do you want to remove the last item (Y/N)";
+        cin>>decision;
+        if(decision='Y'){
+            inventory.pop_back();
         }
-        // int choice;
-        // switch (choice)
-        // {
-        // case 1:
-        //     string ;
-        //     cout<<"Enter item: ";
-        //     break;
-        
-        // default:
-        //     break;
-        // }
+        else if(decision='N'){
+
+        }
+        else{
+            cout<<"Enter a valid desicion"<<endl;
+        }
+    }
+    else{
+        cout<<"Your inventory is empty!"<<endl;
+    }
+        default:
+            break;
+        }
     }
